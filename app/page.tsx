cfd91624ea6061651d72ba70013d4654219c22dc
Rahilsiddique <siddiquerahil19@gmail.com>
2023-04-08 23:22:28 +0530

    temp post added
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import fs from 'fs'
+
+const getPostMetadata = () => {
+  const folder = 'posts/'
+  const files = fs.readdirSync(folder)
+  const markdownPosts = files.filter(file => file.endsWith('.md'))
+  const slug = markdownPosts.map(file => file.replace('.md',''))
+  return slug
+}
 
 const page = () => {
+  const postMetadata = getPostMetadata()
+  const postPreviews = postMetadata.map(slug => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ))
   return (
-    <div>page</div>
+    <div>{postPreviews}</div>
   )
 }
 
